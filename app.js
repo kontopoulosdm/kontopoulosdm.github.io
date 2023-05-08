392cdcb11ad94a695706c88fbb573fda6d824d94
Dimitris Kontopoulos <kontopoulosdm@gmail.com>
2023-05-08 10:19:53 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const https = require("https");
+const express = require("express");
+const cors = require('cors');
+
+//load the config files which contains our hub_ids and their urls
+const config = require('./config');
+
+const app = express();
+app.use(cors());
+app.get( 
+        "/myscripts",
+        async (req, res) => {
+              let result = {} 
+              try{ result.success = true;}
+              catch(e){ result.success = false;} 
+              finally{                 
+                 for (var hubObj of config.hubsarray){
+                     if(hubObj.hub_id == myHub){
+                        myUrls = hubObj.urls;
+                        res.send(myUrls);
+                        break; 
+                     } 
+                 } 
+                 if(myUrls == ""){ 
+                        res.send("noUrls");
+                 }
+          }
+    });
+
+var httpServer = http.createServer(app);
+
+httpServer.listen(3000, () => 
+  	console.log("HTTP Server running on port 3000")
+);
---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.hubsarray = [
+    'https://user.github.io/githubrepo/components/component1.js,https://user.github.io/githubrepo/components/component2.js,https://user.github.io/githubrepo2/components/component3.js,https://user.github.io/githubrepo3/components/component5.js,https://user.github.io/githubrepo4/components/component6.js,https://user2.github.io/githubrepo5/components/script1.js',
+	'https://user2.github.io/githubrepo5/components/component1.js,https://user2.github.io/githubrepo5/components/component2.js',
+	'https://user1.github.io/githubrepo3/components/component1.js,https://user2.github.io/githubrepo5/components/component2.js'
+];
+
+module.exports = config;
